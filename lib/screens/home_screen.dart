d94665e18ac0a11276a8a921d25d8391afbd3566
Seayurre <yulyul03@daum.net>
2025-04-24 17:24:47 +0900

    wake word implementation via porcupine - complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,14 +11,16 @@ import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
 import 'package:path/path.dart' as p;
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
-import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:porcupine_flutter/porcupine.dart';
+import 'package:porcupine_flutter/porcupine_error.dart';
+import 'package:porcupine_flutter/porcupine_manager.dart';
 import 'package:voice_recognition/services/upload_service.dart';
 
 enum SelectedSituation { calibration, motionGeneration, viewMotion }
 
 const WAKE_WORD = "가나다라";
+const PORCUPINE_ACCESS_KEY =
+    "GQukfKf1TefwCCBynTFATbYrW7LTJyg6FAveMQ0opj8Prmi6iyrATA==";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -37,30 +39,28 @@ class _HomeScreenState extends State<HomeScreen> {
   String audioPath = ''; //녹음중단 시 경로 받아올 변수
   String playAudioPath = ''; //저장할때 받아올 변수 , 재생 시 필요
 
+  //Porcupine
+  late PorcupineManager _porcupineManager;
+
   //noise meter 패키지에서 필요한 것들 (from example code)
   NoiseReading? _latestReading;
   StreamSubscription<NoiseReading>? _noiseSubscription;
   NoiseMeter? noiseMeter;
-  bool isNoiseMeterRecording = false;
 
-  //speech_to_text 패키지에서 필요한 것들
-  final SpeechToText _speechToText = SpeechToText(); //SpeechToText Object
-  bool _sttEnabled = false;
-  String _sttText = '';
+  DateTime? _belowThresholdStart;
 
   @override
   void initState() {
     super.initState();
-    //initRecorder();
-    _initSTT();
-    //noiseMeterStart();
+    initRecorder();
+    initPorcupine();
+    noiseMeterStart();
   }
 
   @override
   void dispose() {
     recorder.closeRecorder();
-    noiseMeterStop();
-    _stopSTT();
+    stopPorcupine();
     super.dispose();
   }
 
@@ -69,23 +69,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         SizedBox(height: 10),
-        // TextButton(
-        //   onPressed: () async {
-        //     if (isNoiseMeterRecording) {
-        //       noiseMeterStop();
-        //     } else {
-        //       await noiseMeterStart();
-        //     }
-        //     setState(() {});
-        //   },
-        //   child: Container(
-        //     padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        //     child: Text(
-        //       isNoiseMeterRecording ? 'Recording Off' : 'Recording On',
-        //       style: TextStyle(fontSize: 20),
-        //     ),
-        //   ),
-        // ),
 
         //recorder가 켜져 있는지 아닌지 알려주는 것
         Container(
@@ -100,7 +83,6 @@ class _HomeScreenState extends State<HomeScreen> {
             'Noise: ${_latestReading?.meanDecibel.toStringAsFixed(2)} dB',
           ),
         ),
-        Text('Max: ${_latestReading?.maxDecibel.toStringAsFixed(2)} dB'),
 
         //radio buttons
         ListTile(
@@ -140,64 +122,8 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
         ),
-        GestureDetector(
-          onTap: () async {
-            if (_speechToText.isNotListening) {
-              await _startSTT();
-            } else {
-              _stopSTT();
-            }
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-            child: Text(
-              _speechToText.isNotListening ? 'Start STT' : 'Stop STT',
-            ),
-          ),
-        ),
-        SizedBox(height: 16),
-        Text('stt word: $_sttText'),
       ],
     );
-  }
-
-  void noiseMeterOnData(NoiseReading noiseReading) async {
-    setState(() {
-      _latestReading = noiseReading;
-    });
-    if (isRecording && noiseReading.meanDecibel < 50) {
-      await Future.delayed(Duration(milliseconds: 2000));
-      //recorder 정지 및 파일 전송
-      final path = await recorder.stopRecorder();
-      audioPath = path!;
-
-      setState(() {
-        isRecording = false;
-      });
-
-      final savedFilePath = await saveRecordingLocally();
-      print("savedFilePath: $savedFilePath");
-      UploadService uploadService = UploadService();
-      await uploadService.uploadAudioFile(
-        audioPath,
-        'mp3',
-        _selectedSituation == SelectedSituation.calibration
-            ? "calibration"
-            : _selectedSituation == SelectedSituation.motionGeneration
-            ? "motionGeneration"
-            : "viewMotion",
-      );
-      noiseMeterStop();
-    }
-    setState(() {});
-  }
-
-  void onError(Object error) {
-    print(error);
   }
 
   Future<bool> checkPermission() async => await Permission.microphone.isGranted;
@@ -212,87 +138,101 @@ class _HomeScreenState extends State<HomeScreen> {
     recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
   }
 
+  void initPorcupine() async {
+    await createPorcupineManager();
+    await _porcupineManager.start();
+  }
+
+  void stopPorcupine() async {
+    await _porcupineManager.stop();
+    await _porcupineManager.delete();
+  }
+
+  Future<void> createPorcupineManager() async {
+    try {
+      _porcupineManager = await PorcupineManager.fromBuiltInKeywords(
+        PORCUPINE_ACCESS_KEY,
+        [BuiltInKeyword.ALEXA],
+        _wakeWordCallback,
+      );
+    } on PorcupineException catch (err) {
+      print(err);
+      print("=========An Error Occured========");
+    }
+  }
+
+  void _wakeWordCallback(int keywordIndex) async {
+    if (keywordIndex == 0) {
+      // porcupine detected
+      await record();
+    } else {
+      print("뭔가 잘못됐다");
+    }
+  }
+
+  void onNoiseMeterData(NoiseReading noiseReading) async {
+    if (isRecording && noiseReading.meanDecibel < 50) {
+      if (_belowThresholdStart == null) {
+        _belowThresholdStart = DateTime.now();
+      } else {
+        final duration = DateTime.now().difference(_belowThresholdStart!);
+        if (duration >= const Duration(seconds: 1)) {
+          await stop();
+          UploadService uploadService = UploadService();
+          await uploadService.uploadAudioFile(
+            audioPath,
+            'mp3',
+            _selectedSituation == SelectedSituation.calibration
+                ? "calibration"
+                : _selectedSituation == SelectedSituation.motionGeneration
+                ? "motionGeneration"
+                : "viewMotion",
+          );
+          _belowThresholdStart = null;
+        }
+      }
+    } else {
+      _belowThresholdStart = null;
+    }
+    setState(() {
+      _latestReading = noiseReading;
+    });
+  }
+
+  void onNoiseMeterError(Object error) {
+    print(error);
+    stop();
+  }
+
   Future<void> noiseMeterStart() async {
     noiseMeter ??= NoiseMeter();
     if (!(await checkPermission())) await requestPermission();
     _noiseSubscription = noiseMeter?.noise.listen(
-      noiseMeterOnData,
-      onError: onError,
+      onNoiseMeterData,
+      onError: onNoiseMeterError,
     );
-    setState(() => isNoiseMeterRecording = true);
   }
 
   void noiseMeterStop() {
     _noiseSubscription?.cancel();
-    setState(() => isNoiseMeterRecording = false);
   }
 
-  void _initSTT() async {
+  Future<void> record() async {
     if (!(await checkPermission())) await requestPermission();
-    _sttEnabled = await _speechToText.initialize(
-      onError: errorListener,
-      onStatus: statusListener,
-    );
-    print('Speech enabled: $_sttEnabled');
-    setState(() {});
+    await recorder.startRecorder(toFile: 'audio'); //codec 종류도 바꿀 수 있는 듯
+    setState(() => isRecording = true);
   }
 
-  Future _startSTT() async {
-    debugPrint("========================debug print=====================");
-    await _speechToText.listen(
-      listenFor: Duration(seconds: 10),
-      onResult: _onSpeechResult,
-      localeId: 'ko_KR',
-      listenOptions: SpeechListenOptions(
-        cancelOnError: false,
-        partialResults: true,
-        listenMode: ListenMode.dictation,
-      ),
-    );
+  Future<void> stop() async {
+    final path = await recorder.stopRecorder();
+    audioPath = path!;
+
     setState(() {
-      _sttEnabled = true;
+      isRecording = false;
     });
-  }
 
-  void _stopSTT() async {
-    await _speechToText.stop();
-    print('stop STT');
-    setState(() {
-      _sttEnabled = false;
-    });
-  }
-
-  void _onSpeechResult(SpeechRecognitionResult result) async {
-    print('-------speech result--------');
-    print(result.recognizedWords);
-    setState(() {
-      _sttText = result.recognizedWords;
-    });
-    // if (result.recognizedWords == WAKE_WORD) {
-    //   //완전히 동일하긴 어려우니 어느 정도 relaxation을 두어야 하는데...
-    //   print("으으아아ㅓㄻ나ㅣ어라ㅣㅁ너");
-    //   _stopSTT();
-    //   await recorder.startRecorder(toFile: 'audio');
-    //   noiseMeterStart();
-    //   setState(() => isRecording = true);
-    // }
-  }
-
-  void errorListener(SpeechRecognitionError error) async {
-    debugPrint(error.errorMsg.toString());
-    await Future.delayed(Duration(milliseconds: 500));
-  }
-
-  void statusListener(String status) async {
-    debugPrint("status $status");
-    if (status == "done" && _sttEnabled) {
-      //done이면 다시 시작
-      setState(() {
-        _sttText = "";
-        _sttEnabled = false;
-      });
-      await _startSTT();
-    }
+    final savedFilePath = await saveRecordingLocally();
+    print("savedFilePath: $savedFilePath");
   }
 
   Future<String> saveRecordingLocally() async {
